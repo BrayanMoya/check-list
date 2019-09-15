bada4a09e97dea0510a463e660c81d6565b2000d
bmoya17 <bmoya17@hotmail.com>
2019-09-15 00:56:50 -0500

    Modulo checklist funcionando

--- apps/frontend/modules/checkList/templates/indexSuccess.php
+++ apps/frontend/modules/checkList/templates/indexSuccess.php
@@ -1,34 +1,61 @@
-<h1>Check lists List</h1>
+<div id="content">
+  <div class="container-fluid">
+    <div class="row-fluid">
+      <div class="span12">
+        <div class="widget-box">
+          <div class="widget-title">
+            <span class="icon"><i class="icon-th"></i></span>
+            <h5>Listado de CheckList</h5>
+          </div>
+          <div class="widget-content nopadding">
+            <div id="DataTables_Table_0_wrapper" class="dataTables_wrapper" role="grid">
+              <div class="">
+              </div>
+              <table class="table table-bordered data-table dataTable">
+                <thead>
+                  <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Observations</th>
+                    <th>Reference</th>
+                    <th>Template</th>
+                    <th>Responsible</th>
+                    <th>Original threshold</th>
+                    <th>Assessment</th>
+                    <th>Status</th>
+                    <th>Version at</th>
+                    <th>Created at</th>
+                    <th>Updated at</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php foreach ($pager->getResults() as $check_list) : ?>
+                    <tr>
+                      <td><a href="<?php echo url_for('checkList/show?id=' . $check_list->getId()) ?>"><?php echo $check_list->getId() ?></a></td>
+                      <td><?php echo $check_list->getName() ?></td>
+                      <td><?php echo $check_list->getObservations() ?></td>
+                      <td><?php echo $check_list->getReference() ?></td>
+                      <td><?php echo $check_list->getTemplateId() ?></td>
+                      <td><?php echo $check_list->getResponsibleId() ?></td>
+                      <td><?php echo $check_list->getOriginalThreshold() ?></td>
+                      <td><?php echo $check_list->getAssessment() ?></td>
+                      <td><?php echo $check_list->getStatus() ?></td>
+                      <td><?php echo $check_list->getVersionAt() ?></td>
+                      <td><?php echo $check_list->getCreatedAt() ?></td>
+                      <td><?php echo $check_list->getUpdatedAt() ?></td>
+                    </tr>
+                  <?php endforeach; ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
-<table class="table">
-  <thead>
-    <tr>
-      <th>Id</th>
-      <th>Name</th>
-      <th>Descriptor</th>
-      <th>Threshold</th>
-      <th>Status</th>
-      <th>Version at</th>
-      <th>Created at</th>
-      <th>Updated at</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($pager->getResults() as $check_list): ?>
-    <tr>
-      <td><a href="<?php echo url_for('checkList/show?id='.$check_list->getId()) ?>"><?php echo $check_list->getId() ?></a></td>
-      <td><?php echo $check_list->getName() ?></td>
-      <td><?php echo $check_list->getObservations() ?></td>
-      <td><?php echo $check_list->getOriginalThreshold() ?></td>
-      <td><?php echo $check_list->getStatus() ?></td>
-      <td><?php echo $check_list->getVersionAt() ?></td>
-      <td><?php echo $check_list->getCreatedAt() ?></td>
-      <td><?php echo $check_list->getUpdatedAt() ?></td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
 
 <?php include_partial('pagination', array('pager' => $pager)) ?>
 
-  <a href="<?php echo url_for('checkList/new') ?>">New</a>
+<a href="<?php echo url_for('checkList/new') ?>">New</a>
--- apps/frontend/modules/checkList/templates/showSuccess.php
+++ apps/frontend/modules/checkList/templates/showSuccess.php
@@ -1,47 +1,80 @@
+<div id="content">
+  <div id="content-header">
+  </div>
+  <div class="container-fluid">
+    <div class="row-fluid">
+      <div class="span12">
+        <div class="widget-box">
+          <div class="widget-title">
+            <span class="icon"><i class="icon-th"></i></span>
+            <h5>Listado de CheckList</h5>
+          </div>
+          <div class="widget-content nopadding">
+            <div id="DataTables_Table_0_wrapper" class="dataTables_wrapper" role="grid">
+              <div class="">
+              </div>
+              <table class="table table-bordered data-table dataTable">
+                <tbody>
+                  <tr>
+                    <th>Id:</th>
+                    <td><?php echo $check_list->getId() ?></td>
+                  </tr>
+                  <tr>
+                    <th>Name:</th>
+                    <td><?php echo $check_list->getName() ?></td>
+                  </tr>
+                  <tr>
+                    <th>Observations:</th>
+                    <td><?php echo $check_list->getObservations() ?></td>
+                  </tr>
+                  <tr>
+                    <th>Reference:</th>
+                    <td><?php echo $check_list->getReference() ?></td>
+                  </tr>
+                  <tr>
+                    <th>Template:</th>
+                    <td><?php echo $check_list->getTemplateId() ?></td>
+                  </tr>
+                  <tr>
+                    <th>Responsible:</th>
+                    <td><?php echo $check_list->getResponsibleId() ?></td>
+                  </tr>
+                  <tr>
+                    <th>Original threshold:</th>
+                    <td><?php echo $check_list->getOriginalThreshold() ?></td>
+                  </tr>
+                  <tr>
+                    <th>Assessment:</th>
+                    <td><?php echo $check_list->getAssessment() ?></td>
+                  </tr>
+                  <tr>
+                    <th>Status:</th>
+                    <td><?php echo $check_list->getStatus() ?></td>
+                  </tr>
+                  <tr>
+                    <th>Version at:</th>
+                    <td><?php echo $check_list->getVersionAt() ?></td>
+                  </tr>
+                  <tr>
+                    <th>Created at:</th>
+                    <td><?php echo $check_list->getCreatedAt() ?></td>
+                  </tr>
+                  <tr>
+                    <th>Updated at:</th>
+                    <td><?php echo $check_list->getUpdatedAt() ?></td>
+                  </tr>
+                </tbody>
+              </table>
 
-<table>
-  <tbody>
-    <tr>
-      <th>Id:</th>
-      <td><?php echo $check_list->getId() ?></td>
-    </tr>
-    <tr>
-      <th>Name:</th>
-      <td><?php echo $check_list->getNameWithPrefix() ?></td>
-    </tr>
-    <tr>
-      <th>Descriptor:</th>
-      <td><?php echo $check_list->getDescriptor() ?></td>
-    </tr>
-    <tr>
-      <th>Prefix:</th>
-      <td><?php echo $check_list->getPrefix() ?></td>
-    </tr>
-    <tr>
-      <th>Threshold:</th>
-      <td><?php echo $check_list->getThreshold() ?></td>
-    </tr>
-    <tr>
-      <th>Status:</th>
-      <td><?php echo $check_list->getStatus() ?></td>
-    </tr>
-    <tr>
-      <th>Version at:</th>
-      <td><?php echo $check_list->getVersionAt() ?></td>
-    </tr>
-    <tr>
-      <th>Created at:</th>
-      <td><?php echo $check_list->getCreatedAt() ?></td>
-    </tr>
-    <tr>
-      <th>Updated at:</th>
-      <td><?php echo $check_list->getUpdatedAt() ?></td>
-    </tr>
-  </tbody>
-</table>
+              <hr />
 
-<hr />
-
-<a href="<?php echo url_for('checkList/edit?id='.$check_list->getId()) ?>">Edit</a>
-&nbsp;
-<a href="<?php echo url_for('checkList/index') ?>">List</a>
+              <a href="<?php echo url_for('checkList/edit?id=' . $check_list->getId()) ?>">Edit</a>
+              &nbsp;
+              <a href="<?php echo url_for('checkList/index') ?>">List</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
