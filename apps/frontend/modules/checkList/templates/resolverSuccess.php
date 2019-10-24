2fd8e0b52b9770645c835b901ec3f755810d63ea
bmoya17 <bmoya17@hotmail.com>
2019-10-24 12:54:41 -0500

    Mensaje de advertencia de killer question en formulario embebido agregado

--- apps/frontend/modules/checkList/templates/resolverSuccess.php
+++ apps/frontend/modules/checkList/templates/resolverSuccess.php
@@ -4,43 +4,46 @@
 <?php use_stylesheets_for_form($form) ?>
 <?php use_javascripts_for_form($form) ?>
 
-<form class="m-t-40"
-     action="<?php echo url_for('checkList/resolver?id=' . $form->getObject()->getId()) ?>"
-     method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
-   <?php if (!$form->getObject()->isNew()): ?>
-       <input type="hidden" name="sf_method" value="put"/>
-   <?php endif; ?>
-   <div class="row">
+<form class="m-t-40" action="<?php echo url_for('checkList/resolver?id=' . $form->getObject()->getId()) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
+    <?php if (!$form->getObject()->isNew()) : ?>
+        <input type="hidden" name="sf_method" value="put" />
+    <?php endif; ?>
+    <div class="row">
 
-           <ul class="error_list col-md-12">
-               <?php foreach ($form->getGlobalErrors() as $name => $error): ?>
-                   <li><?php echo $name . ': ' . $error ?></li>
-               <?php endforeach; ?>
-           </ul>
+        <ul class="error_list col-md-12">
+            <?php foreach ($form->getGlobalErrors() as $name => $error) : ?>
+                <li><?php echo $name . ': ' . $error ?></li>
+            <?php endforeach; ?>
+        </ul>
+        <?php $instanceForm = $form->getEmbeddedForm('standardList')?>
+        <!-- iteramos por todos los  formularios embebidos  -->
+        <?php foreach ($form['standardList'] as $key => $subForm) : ?>
+        <?php $instanceForm2 = $instanceForm->getEmbeddedForm($key)?>
+            <?php if ($instanceForm2->getObject()->getIsKillerQuestion()): ?>
+                <div class="form-group col-md-6"> <h6 style="color:red">*Criterio de obligatoria respuesta</h6> </div>
+            <?php endif; ?>
+                <div class="form-group col-md-6"> <?php echo  $subForm['option_selected']->renderLabel()  ?></div>
+                <div class="form-group col-md-4 <?php echo $subForm['option_selected']->hasError() ? 'has-error' : '' ?>">
+                    <?php echo $subForm['option_selected'] ?>
+                    <?php echo $subForm['option_selected']->renderError() ?>
+                </div>
+        <?php endforeach; ?>
 
-           <!-- iteramos por todos los  formularios embebidos  -->
-           <?php foreach ($form['standardList'] as $subForm):?>
-               <div  class="form-group col-md-6"> <?php echo  $subForm['option_selected']->renderLabel()  ?></div>
-               <div class="form-group col-md-4 <?php echo $subForm['option_selected']->hasError() ? 'has-error' : '' ?>">
-                   <?php echo $subForm['option_selected'] ?>
-                   <?php echo $subForm['option_selected']->renderError() ?>
-               </div>
-           <?php endforeach; ?>
+        <div class="col-md-12">
+            <?php echo $form->renderHiddenFields(true) ?>
+        </div>
+        <div class="row">
+            <div class="col m12">
+                &nbsp;<a href="<?php echo url_for('checkList/index') ?>">Volver a la lista</a>
+                <input type="submit" value="Save" />
 
-           <div class="col-md-12">
-               <?php echo $form->renderHiddenFields(true) ?>
-           </div>
-       <div class="row">
-           <div class="col m12">
-               &nbsp;<a href="<?php echo url_for('checkList/index') ?>">Volver a la lista</a>
-               <input type="submit" value="Save"/>
-
-           </div>
-       </div>
+            </div>
+        </div>
+        
 </form>
 <style>
-   form {
-       padding-left: 130px;
-       padding-right: 130px;
-   }
+    form {
+        padding-left: 130px;
+        padding-right: 130px;
+    }
 </style>
